e70435e3da922653231cf8de1f85cbe7e236d573
guillaumepn <guillaumepn@free.fr>
2018-08-01 00:27:35 +0200

    first commit : first layers of components & containers

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <Aux>
+        <Burger/>
+        <div>Build controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;

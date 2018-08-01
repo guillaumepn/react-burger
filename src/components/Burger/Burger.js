db0869844b2bc36d9999b8582723986f76c4c53c
guillaumepn <guillaumepn@free.fr>
2018-08-02 01:16:12 +0200

    add controls & ingredients logic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  let ingredients = Object.keys(props.ingredients)
+    .map(ingredientName => {
+      return [...Array(props.ingredients[ingredientName])].map((_, index) => {
+        return <BurgerIngredient key={ingredientName + index} type={ingredientName}/>
+      });
+    }).reduce((arr, el) => {
+      return arr.concat(el);
+    });
+
+  console.log(ingredients);
+
+  if (ingredients.length === 0) {
+    ingredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top'/>
-      <BurgerIngredient type='cheese'/>
-      <BurgerIngredient type='meat'/>
+      { ingredients }
       <BurgerIngredient type='bread-bottom'/>
     </div>
   );
